21cd5fcc37f6cfc9d3b1a59ad2795880b38c59a6
MdJafirAshraf <mdjafirashraf@gmail.com>
2026-07-18 20:59:31 +0530

    first component

---
+++ crm app/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+    return (
+        <div>
+            <h2>Welcome to My Component</h2>
+            <p>This is my first react component.</p>
+        </div>
+    );
+}
+
+export default Welcome;
