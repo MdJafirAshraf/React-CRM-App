fc1e12faea3718290d94ba2371c1ffd6a4ee5039
MdJafirAshraf <mdjafirashraf@gmail.com>
2026-08-03 23:35:38 +0530

    learn form

--- crm app/src/components/dashboard/ProductCard.jsx
+++ crm app/src/components/dashboard/ProductCard.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 function Usercard({name, age}) {
+
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+
+    function formSubmit(e) {
+        e.preventDefault();
+        console.log(userName, userEmail);
+    }
 
     function saveBtn() {
         alert("Save Button")
@@ -15,6 +25,23 @@ function Usercard({name, age}) {
     return (
         <>
             <div style={{ padding: "10px" }}>
+                <form onSubmit={formSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="Enter user name"
+                        value={userName} 
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="Enter user email"
+                        value={userEmail} 
+                        onChange={(e) => setUserEmail(e.target.value)}
+                    />
+                    <button type="submit">
+                        Submit
+                    </button>
+                </form>
                 <button onClick={saveBtn}>
                     Save
                 </button>
